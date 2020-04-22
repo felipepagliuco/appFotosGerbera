bd720ce9fcf410722d522be26cbc5cc5fd212a27
Felipe Pagliuco <felipepagliuco@yahoo.com.br>
2020-04-22 19:18:46 -0300

    Realizando ajustes iniciais

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import shutil
+from collections import Counter
 
 def copia_todos_arquivos_para_pasta_temporaria(pasta) :
     """
@@ -14,45 +15,37 @@ def copia_todos_arquivos_para_pasta_temporaria(pasta) :
     for (dirpath, dirnames, files) in os.walk(pasta, topdown=True) :
         with os.scandir(dirpath) as it :
             for entry in it :
-            # if img_file.name.endswith('.jpg') and img_file.is_file() :
-            # shutil.copyfile(dirpath, diretorio_temporario)
                 if entry.is_file() :
                     arquivos_encontrados.append(entry.name)
                     old_file_path = os.path.join(dirpath, entry.name)
                     shutil.copy(old_file_path,diretorio_temporario)
 
-            print("Copiando arquivos.....")
+        print   ("Copiando arquivos.....")
+
     with os.scandir(diretorio_temporario) as it :
         for entry in it :
             arquivos_copiados.append(entry.name)
 
     print("Total de arquivos encontrados: ", len(arquivos_encontrados))
     print("Arquivos encontrados: ",arquivos_encontrados)
+    print("Arquivos repetidos = ",[item for item, count in Counter(arquivos_encontrados).items() if count > 1])
     print("Total de arquivos copiados: ", len(arquivos_copiados))
     print("Arquivos copiados: ",arquivos_copiados)
-    lista_final = list(set(arquivos_encontrados) - set(arquivos_copiados))
-    print("Arquivos não copiados: ",lista_final )
-
+    # lista_final = list(set(arquivos_encontrados) - set(arquivos_copiados))
+    # print("Arquivos não copiados: ",lista_final )
     return diretorio_temporario
-
-
-# print(type(lista_de_arquivos))
-# def verifica_se_arquivo_e_jpg(lista_de_arquivos):
-#     for arquivo in lista_de_arquivos:
-#         if img_file.name.endswith('.jpg') and img_file.is_file() :
-#
 
 def valida_se_arquivos_sao_jpg(path):
     arquivos_jpg = []
     arquivos_not_jpg = []
     with os.scandir(path) as it :
         for entry in it :
-            if entry.name.endswith('.jpg') :
+            if entry.name.endswith('.jpg') or entry.name.endswith('.JPG') :
                 arquivos_jpg.append(entry.name)
             else:
                 arquivos_not_jpg.append(entry.name)
     print("Arquivos JPG: ",arquivos_jpg)
     print("Arquivos não são JPG: ",arquivos_not_jpg)
-lista_de_arquivos = copia_todos_arquivos_para_pasta_temporaria("Fotos")
+#lista_de_arquivos = copia_todos_arquivos_para_pasta_temporaria("Fotos")
+lista_de_arquivos = "/home/felipe/workspace/appFotosGerbera/temp_633243"
 valida_se_arquivos_sao_jpg(lista_de_arquivos)
-# lista_arquivos = lista_diretorios("Fotos")
